5337a6490eb19bb9c2f85e4739b37982903cb15d
who cares <mariaaramirez80@gmail.com>
2023-03-05 11:09:31 -0800

    index for navbar, hero, and footer

---
+++ client/src/Container/HomeContainer/index.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+import HeroSection from "./HeroSection";
+
+export default function HomeContainer() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <Footer />
+    </>
+  );
+}
