b454cc3f61b510d3f992fe127f4daaf7b580f1b4
who cares <mariaaramirez80@gmail.com>
2023-03-05 11:17:21 -0800

    made a footer container

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#2980b9",
+        color: "#fff",
+        textAlign: "center",
+        padding: "10px",
+        position: "fixed",
+        left: "0",
+        bottom: "0",
+        width: "100%",
+      }}
+    >
+      Created by Provider Finder
+    </footer>
+  );
+};
+
+export default Footer;
